46d7779b34f21b0bc17923f894ccab18bdf1eae7
Frederic Langlet <flanglet@gmail.com>
2022-07-25 16:34:35 -0700

    Fix name

--- java/src/test/java/kanzi/test/TestCompressedStream.java
+++ java/src/test/java/kanzi/test/TestCompressedStream.java
@@ -32,7 +32,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
-public class TestCompressedtStream
+public class TestCompressedStream
 {
    private static final ExecutorService pool = Executors.newFixedThreadPool(4);
 
