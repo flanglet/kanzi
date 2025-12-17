2bcaacaaff5c25155edcbdf56e497433799a42a4
Andreas Reichel <andreas@manticore-projects.com>
2025-12-17 08:12:32 +0700

    style: apply consistent License headers
    - no code changes, only formatting
    - activates `checkLicenses` task
    
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- java/src/main/java/io/github/flanglet/kanzi/transform/Sequence.java
+++ java/src/main/java/io/github/flanglet/kanzi/transform/Sequence.java
@@ -1,10 +1,13 @@
 /*
- * Copyright 2011-2025 Frederic Langlet
+ * Kanzi is a modern, modular, portable, and efficient lossless data compressor.
+ *
+ * Copyright (C) 2011-2025 Frederic Langlet
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
- *                 http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
